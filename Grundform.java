33a5fad0b089d6b1d864d3bb9fbf8ecb0a46c615
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-04-04 14:02:03 +0200

    Grundform und Kreis ergänzt.

---
+++ Grundform.java
@@ -0,0 +1,10 @@
+public class Grundform /* extends Object */ {
+
+    public double berechneFläche() {
+        return 0.0;
+    }
+
+    public double berechneUmfang() {
+        return 0.0;
+    }
+}
---
+++ Kreis.java
@@ -0,0 +1,18 @@
+public class Kreis extends Grundform {
+
+    private double radius;
+
+    public Kreis(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double berechneFläche() {
+        return radius * radius * Math.PI;
+    }
+
+    @Override
+    public double berechneUmfang() {
+        return 2 * radius * Math.PI;
+    }
+}
